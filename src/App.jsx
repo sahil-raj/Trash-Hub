1f36cb4ab466b8e4c8d7f8fd547d53973cb6fe35
sahil-raj <sahilrajaaru@gmail.com>
2024-09-14 02:27:19 +0530

    Aim

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Aim from "./components/Aim";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Aim></Aim>
       <Footer />
     </>
   );
