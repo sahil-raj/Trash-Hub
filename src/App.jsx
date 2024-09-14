ba52e63de2989747b7dec4970e154a2334e57430
sahil-raj <sahilrajaaru@gmail.com>
2024-09-14 11:09:57 +0530

    QRCard added

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Aim from "./components/Aim";
 import ContactUs from "./components/ContactUs";
 import Home from "./components/Home";
 import AboutUs from "./components/Aboutus";
+import QRCard from "./components/QRCard";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home/>
-      <AboutUs/>
+      <Home />
+      <AboutUs />
       <Aim></Aim>
       <ContactUs></ContactUs>
+      <QRCard qrData="test" qrHeading="test QR" qrText="lorem ipsum" />
       <Footer />
     </>
   );
---
+++ src/components/QRCard.jsx
@@ -0,0 +1,45 @@
+import {
+  Card,
+  CardBody,
+  Stack,
+  Text,
+  Heading,
+  Button,
+  CardFooter,
+  Divider,
+  ButtonGroup,
+} from "@chakra-ui/react";
+import { QRCodeSVG } from "qrcode.react";
+
+const QRCard = ({ qrHeading, qrText, qrData, styleClass }) => {
+  return (
+    <Card maxW="sm" align="center" textAlign="center">
+      <CardBody>
+        <QRCodeSVG
+          value={qrData}
+          className={styleClass ? styleClass : "w-12 h-12"}
+        />
+        <Stack mt="6" spacing="3">
+          <Heading size="md">{qrHeading ? qrHeading : ""}</Heading>
+          <Text>{qrText ? qrText : ""}</Text>
+        </Stack>
+      </CardBody>
+      <Divider width="90%" />
+      <CardFooter>
+        <ButtonGroup spacing="2">
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(qrData);
+            }}
+            variant="ghost"
+            colorScheme="blue"
+          >
+            Copy
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default QRCard;
